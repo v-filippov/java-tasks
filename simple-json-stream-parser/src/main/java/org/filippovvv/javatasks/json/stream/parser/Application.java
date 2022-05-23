f0912d8ae98722032e8b597a366bde068774c02e
filippovvv <filippovvv@gmail.com>
2022-05-23 10:59:57 +0300

    finding the given key in the stream implemeneted

--- simple-json-stream-parser/src/main/java/org/filippovvv/javatasks/json/stream/parser/Application.java
+++ simple-json-stream-parser/src/main/java/org/filippovvv/javatasks/json/stream/parser/Application.java
@@ -1,12 +1,18 @@
 package org.filippovvv.javatasks.json.stream.parser;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class Application {
     public static void main(String[] args) {
-        final InputStream inputStream = System.in;
-        final Parser parser = new Parser(inputStream);
-        parser.findByKey("key");
-
+        final Parser parser;
+        try (InputStream inputStream = new FileInputStream("src/main/resources/test.json")) {
+            parser = new Parser(inputStream);
+            parser.findByKey("key");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- simple-json-stream-parser/src/main/java/org/filippovvv/javatasks/json/stream/parser/Parser.java
+++ simple-json-stream-parser/src/main/java/org/filippovvv/javatasks/json/stream/parser/Parser.java
@@ -3,24 +3,70 @@ package org.filippovvv.javatasks.json.stream.parser;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class Parser {
     final InputStream in;
+    final char DOUBLE_QUOTE = '"';
+    final char COLON = ':';
+
     public Parser(final InputStream inputStream) {
         this.in = inputStream;
     }
+
     public String findByKey(final String key) {
         System.out.println("key: " + key);
-        InputStreamReader reader = new InputStreamReader(in);
         int ch = -1;
-        while (true) {
-            try {
-                if (!((ch = reader.read()) != -1)) break;
-            } catch (IOException e) {
-                e.printStackTrace();
+        int[] buffer = new int[key.length()];
+        try (InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8)) {
+            while ((ch = reader.read()) != -1) {
+                System.out.println("Char: " + (char)ch);
+                if (DOUBLE_QUOTE == ch) {
+                    System.out.println("quote found");
+                    if (captureString(reader, key)) {
+                        System.out.println("Key " + key + " found, extracting its value");
+                        extractKeyValue(reader);
+                    }
+                }
             }
-            System.out.println("Char: " + ch);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
         return key;
+    }
+
+    private void extractKeyValue(InputStreamReader reader) {
+
+    }
+
+    private boolean captureString(InputStreamReader reader, String key) {
+        System.out.println("Capturing string");
+        int ch = -1;
+        try {
+            int i = 0;
+            int keyLength = key.length();
+
+            while ( i < keyLength && ((ch = reader.read()) != -1) && key.charAt(i) == (char)ch && (char)ch != DOUBLE_QUOTE) {
+                System.out.println("  ch: " + (char)ch + ", i: " + i);
+                i++;
+            }
+            if (i == keyLength && (char)reader.read() == DOUBLE_QUOTE) {
+                System.out.println("match");
+                while ((ch = reader.read()) != -1 && isWhitespaceOrTab((char)ch)) {
+                    continue;
+                }
+                System.out.println("Char after the second quote: " + (char)ch);
+                if (COLON == (char) ch) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return false;
+    }
+
+    private boolean isWhitespaceOrTab(char ch) {
+        return ch == ' ' || ch == '\t' || ch == '\n';
     }
 }
